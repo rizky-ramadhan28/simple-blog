582a5925ccb8ddbfd7969e83295b400905aacfb2
Rizky Ramadhan <freeebook05@gmail.com>
2020-06-03 04:59:32 +0700

    Updates

---
+++ precache-manifest.2c7c88160105c863785ab0a5a8093aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9142209ac0d4d6cd07b93e46349d1d5",
+    "url": "/simple-blog/index.html"
+  },
+  {
+    "revision": "25e72d354a01b11f6e2a",
+    "url": "/simple-blog/static/css/main.e9b36ee4.chunk.css"
+  },
+  {
+    "revision": "97fb7e2f0836ce814ac8",
+    "url": "/simple-blog/static/js/2.e7ac10c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-blog/static/js/2.e7ac10c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25e72d354a01b11f6e2a",
+    "url": "/simple-blog/static/js/main.d6553fd9.chunk.js"
+  },
+  {
+    "revision": "97186a0ab40c7c6588ea",
+    "url": "/simple-blog/static/js/runtime-main.49ddb3ca.js"
+  }
+]);
